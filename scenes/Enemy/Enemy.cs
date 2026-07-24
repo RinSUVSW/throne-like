3c6dd148238ef4ba68049634a04308504c297daa
Rin Wilde <81784876+RinSUVSW@users.noreply.github.com>
2026-07-24 17:24:43 +0100

    Enemy hording behaviour
    -buggy with lots of enemies but should be fine if enemies spawn aggro to the player

--- scenes/Enemy/Enemy.cs
+++ scenes/Enemy/Enemy.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using Godot;
 
 public struct MovementHeft
@@ -33,67 +34,149 @@ public partial class Enemy : CharacterBody2D
 
 	//[Export]
 	public List<MovementHeft> Heft;
+	public List<Enemy> Allies;
 
 	[Export]
 	public Player player;
 
 	//public const float JumpVelocity = -400.0f;
+	public float m_maxSpeed;
 	private float m_Speed;
 	private Vector2 MovementVector;
+	public int m_size;
+	public float m_MaxHealth;
+	public float m_Health;
+
+	public override void _Ready()
+	{
+		base._Ready();
+		m_size = 5;
+		m_Speed = 75.0f;
+		//m_Speed = m_maxSpeed;
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
+		Heft = new List<MovementHeft>();
+		Allies = new List<Enemy>();
+
 		foreach (Node2D body in Visionbox.GetOverlappingBodies())
 		{
-			if (body is Player)
+			if (!aggro)
 			{
-				aggro = true;
-				player = (Player)body;
-
-				if (Heft == null)
+				if (body is Player)
 				{
-					Heft = new List<MovementHeft>();
-
-					MovementHeft testHeft = new MovementHeft();
-
-					testHeft.targetType = MovementHeft.TargetType.Player;
-					testHeft.weight = 1;
-					testHeft.direction = (player.GlobalPosition - this.GlobalPosition).Normalized();
-
-					Heft.Add(testHeft);
+					aggro = true;
+					player = (Player)body;
 				}
-				else
+			}
+
+			if (player != null)
+			{
+				MovementHeft testHeft = new MovementHeft();
+
+				testHeft.targetType = MovementHeft.TargetType.Player;
+				testHeft.weight = 2f;
+				testHeft.direction = (player.GlobalPosition - this.GlobalPosition).Normalized();
+
+				Heft.Add(testHeft);
+			}
+
+			if (body is Enemy)
+			{
+				Allies.Add((Enemy)body);
+
+				//check if too close
+
+				float XDiff = GlobalPosition.X - body.GlobalPosition.X;
+				float YDiff = GlobalPosition.Y - body.GlobalPosition.Y;
+
+				float distanceApart = MathF.Sqrt((XDiff * XDiff) + (YDiff * YDiff));
+
+				if (distanceApart <= m_size + ((Enemy)body).m_size)
 				{
-					MovementHeft testHeft2 = Heft[0];
+					//too close
+					MovementHeft tooCloseHeft = new MovementHeft();
 
-					testHeft2.direction = (player.GlobalPosition - this.GlobalPosition).Normalized();
+					tooCloseHeft.targetType = MovementHeft.TargetType.AntiClump;
+					tooCloseHeft.weight = 4.0f;
+					tooCloseHeft.direction = (this.GlobalPosition - body.GlobalPosition).Normalized();
 
-					Heft[0] = testHeft2;
+					Heft.Add(tooCloseHeft);
+
+					//GD.Print("Too Close");
 				}
 			}
 		}
 
-		if (player != null)
+		if (Allies.Count > 0)
 		{
-			GD.Print("I See the player");
+			Vector2 totalPosition = Vector2.Zero;
+
+			for (int i = 0; i < Allies.Count; i++)
+			{
+				if (aggro)
+				{
+					if (!Allies[i].aggro)
+					{
+						Allies[i].aggro = true;
+						Allies[i].player = player;
+					}
+				}
+
+				totalPosition += Allies[i].GlobalPosition;
+			}
+			Vector2 averagePosition = totalPosition / Allies.Count;
+
+			MovementHeft hordeHeft = new MovementHeft();
+
+			hordeHeft.targetType = MovementHeft.TargetType.Horde;
+			hordeHeft.weight = 0.5f;
+			hordeHeft.direction = (averagePosition - this.GlobalPosition).Normalized();
+
+			float XDiff2 = averagePosition.X - GlobalPosition.X;
+			float YDiff2 = averagePosition.Y - GlobalPosition.Y;
+
+			float distanceApart2 = MathF.Sqrt((XDiff2 * XDiff2) + (YDiff2 * YDiff2));
+
+			if (distanceApart2 < m_size * 2 || aggro)
+			{
+				hordeHeft.weight = -0.0f;
+			}
+
+			Heft.Add(hordeHeft);
 		}
 
 		Vector2 velocity = Velocity;
 
 		Vector2 direction = Vector2.Zero;
 
-		if (Heft != null)
+		if (Heft.Count != 0)
 		{
-			direction = Heft[0].direction;
+			foreach (MovementHeft mh in Heft)
+			{
+				direction += mh.direction * mh.weight;
+			}
+			if (direction.Length() >= 1.0f)
+			{
+				direction = direction.Normalized();
+			}
+		}
+		else
+		{
+			direction = Vector2.Zero;
 		}
 
 		if (direction != Vector2.Zero)
 		{
-			velocity.X = direction.X * Speed;
+			//m_Speed = m_maxSpeed;
+
+			velocity = direction * m_Speed;
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			//m_Speed = Mathf.Lerp(m_Speed, 0, 0.8f);
+			velocity = Vector2.Zero;
 		}
 
 		Velocity = velocity;
